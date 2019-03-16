e98e31a8940589b6f8a7ca73b1dde020821145b4
ilya-korotya <officalbatya@gmail.com>
2019-03-17 00:26:26 +0200

    Error handling in HTTP controller

--- handler/users.go
+++ handler/users.go
@@ -10,11 +10,12 @@ import (
 func users(c *server.Context) error {
 	users, err := c.UserUsecase.Users()
 	if err != nil {
-		return err
+		return c.ProcessError(err)
 	}
 	return c.Response(http.StatusOK, users)
 }
 
+// use for check graceful shutdown
 func foo(c *server.Context) error {
 	time.Sleep(30 * time.Second)
 	return c.Response(http.StatusOK, map[string]string{"message": "OK"})
